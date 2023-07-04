4329999762593875dc60ee9913ddbbd07293c1fc
hrithikscube <hrithik@scube.me>
2023-07-04 09:02:57 +0530

    cosmic abyss

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { content } from './util/helpers'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <div className='lg:w-[720px] lg:m-auto flex flex-col m-5'>
+
+        <div className='flex flex-col mt-10'>
+          <h1 className='font-sans lg:text-4xl md:text-4xl text-2xl font-bold'>Exploring the Enigmatic Abyss: What Are Black Holes?</h1>
+
+          <p className='lg:text-xl md:text-lg text-base font-serif font-light mt-5 leading-relaxed tracking-wide'>
+            In the vast expanse of space, amidst the celestial wonders and cosmic mysteries, black holes stand as enigmatic powerhouses that both captivate and challenge our understanding of the universe. These cosmic abysses, with their immense gravitational pull and mind-bending properties, have long intrigued scientists and fascinated stargazers. In this article, we embark on a journey to unravel the mysteries surrounding black holes and shed light on what they truly are.
+          </p>
+        </div>
+
+        {
+          content.map((item, index) => (
+            <div key={index + 1 + item.name} className='mt-5'>
+              <h2 className='lg:text-2xl text-xl font-bold'>{item.name}</h2>
+
+              <p className='lg:text-xl md:text-lg text-base font-serif font-light mt-5 leading-relaxed tracking-wide'>
+                {item.description}
+              </p>
+            </div>
+          ))
+        }
+        <br />
+
+      </div>
+      <div className='w-full py-2 flex justify-center items-center bg-[#f2f2f2]'>
+        <h3 className='lg:text-base font-medium'>Cosmic Abyss - Exploring the Universe's Secrets | © 2023 Cosmic Abyss. All rights reserved.</h3>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ src/util/helpers.js
@@ -0,0 +1,26 @@
+export const content = [
+    {
+        name: "The Birth of a Black Hole:",
+        description: "Black holes form when massive stars reach the end of their life cycle. When such a star exhausts its nuclear fuel, it undergoes a catastrophic collapse under its own gravitational force. This collapse is so extreme that it compresses the star's matter into an infinitesimally small volume, creating a region of space where gravity becomes immensely strong. This point of infinite density is known as a singularity, and it is surrounded by an event horizon, the boundary beyond which nothing, not even light, can escape."
+    },
+    {
+        name: "Gravity's Grasp:",
+        description: "What sets black holes apart from other celestial objects is their gravity. Their gravitational pull is so intense that it distorts space and time, creating a phenomenon known as spacetime curvature. This distortion forms a gravitational well, drawing nearby matter and even light into its embrace. It is this gravitational grip that lends black holes their name, as they appear as voids in the fabric of space, absorbing everything that crosses their event horizon."
+    },
+    {
+        name: "Types of Black Holes:",
+        description: "Black holes come in different sizes and can be categorized into three main types: stellar black holes, intermediate-mass black holes, and supermassive black holes. Stellar black holes are formed from the collapse of massive stars and have a mass several times that of our sun. Intermediate-mass black holes fall between stellar black holes and supermassive black holes, ranging from thousands to millions of solar masses. Supermassive black holes, on the other hand, reside at the centers of galaxies and have masses equivalent to millions or even billions of suns."
+    },
+    {
+        name: "Unveiling the Secrets:",
+        description: "While the inner workings of black holes remain shrouded in mystery, scientists have made remarkable strides in understanding their behavior. Through a combination of theoretical models and observations, researchers have unveiled intriguing phenomena associated with black holes. These include gravitational time dilation, where time appears to slow down near a black hole, and the emission of powerful jets of high-energy particles from its vicinity."
+    },
+    {
+        name: "The Role of Black Holes:",
+        description: "Black holes play a significant role in shaping the cosmos. They are thought to have a profound impact on the evolution of galaxies, as their immense gravitational forces can influence the motion and distribution of stars and gas. Furthermore, black holes provide a unique window into the fundamental laws of physics, allowing scientists to test theories such as general relativity and explore the boundaries of our understanding."
+    },
+    {
+        name: "Conclusion:",
+        description: "Black holes, the cosmic abysses that have intrigued and puzzled humanity for decades, continue to be objects of intense scientific research and exploration. From their awe-inspiring formation to their gravitational dominance, these enigmatic entities push the boundaries of our knowledge and inspire a sense of wonder. As we venture further into the depths of the universe, the study of black holes promises to unveil new insights and unlock the secrets of the cosmos, bringing us closer to understanding the intricate workings of the universe we call home."
+    },
+]
